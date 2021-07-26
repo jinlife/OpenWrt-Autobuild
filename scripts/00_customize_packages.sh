ccc0593d4f82ec2febf84f044d41b6e0a6dee3c4
jinlife <glucose1e@tom.com>
2021-07-26 08:50:29 +0800

    change code

--- scripts/00_customize_packages.sh
+++ scripts/00_customize_packages.sh
@@ -95,33 +95,4 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/vlmcsd package/ne
 # xlnetacc
 git clone -b master https://github.com/garypang13/luci-app-xlnetacc.git package/new/luci-app-xlnetacc
 
-# Edge Theme
-git clone -b master https://github.com/garypang13/luci-theme-edge.git package/new/luci-theme-edge
-
-# poweroff
-git clone -b master https://github.com/esirplayground/luci-app-poweroff package/new/luci-app-poweroff
-# Socat IPv6 to IPv4
-svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-socat package/new/luci-app-socat
-
-# Add r8125 realtek network card driver
-# svn co https://github.com/Lienol/openwrt/branches/19.07/package/kernel/r8125 package/new/r8125
-# svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/r8125 package/new/r8125
-cp -rf ../r8125 package/new/r8125
-
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/r8152 package/new/r8152
-
-#Default IP
-sed -i 's#192.168.1.1#192.168.0.1#g' package/base-files/files/bin/config_generate
-#net.netfilter.nf_conntrack_max
-sed -i 's#net.netfilter.nf_conntrack_max=16384#net.netfilter.nf_conntrack_max=65535#g' package/kernel/linux/files/sysctl-nf-conntrack.conf
-
-# default settings and translate
-cp -rf ../default-settings package/new/lean-translate
-
-ZZZ="package/new/lean-translate/files/zzz-default-settings"
-# Modify machine name to OpenWrt-X86
-sed -i "/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-X86'" $ZZZ
-# Customized name
-sed -i "s/OpenWrt /Jinlife build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ
-
 exit 0
