05a2be8cb318d4b8364955abfd9de695e9128256
jinlife <glucose1e@tom.com>
2021-07-26 09:04:53 +0800

    garypang13 changed name to kiddin9

--- scripts/00_customize_packages.sh
+++ scripts/00_customize_packages.sh
@@ -93,6 +93,6 @@ ln -sf ../../../feeds/luci/applications/luci-app-vlmcsd ./package/feeds/luci/luc
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/vlmcsd package/new/vlmcsd
 
 # xlnetacc
-git clone -b master https://github.com/garypang13/luci-app-xlnetacc.git package/new/luci-app-xlnetacc
+git clone -b master https://github.com/kiddin9/luci-app-xlnetacc.git package/new/luci-app-xlnetacc
 
 exit 0
