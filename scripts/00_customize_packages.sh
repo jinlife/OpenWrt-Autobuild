360f71d813b76e0284055fcef97cb259af8bbd88
jinlife <glucose1e@tom.com>
2021-12-05 17:35:22 +0800

    Remove unstable passwall

--- scripts/00_customize_packages.sh
+++ scripts/00_customize_packages.sh
@@ -11,7 +11,7 @@ cp -rf ../luci-app-adguardhome package/new/luci-app-adguardhome
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-arpbind package/new/luci-app-arpbind
 
 # AutoCore
-cp -rf ../autocore package/new/autocore
+svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/emortal/autocore package/new/autocore
 
 # cpufreq
 svn co https://github.com/immortalwrt/luci/branches/openwrt-21.02/applications/luci-app-cpufreq feeds/luci/applications/luci-app-cpufreq
@@ -24,24 +24,12 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-lib-fs packa
 # FullCone
 svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/kernel/fullconenat package/network/fullconenat
 wget -P target/linux/generic/hack-5.4/ https://github.com/immortalwrt/immortalwrt/raw/openwrt-21.02/target/linux/generic/hack-5.4/952-net-conntrack-events-support-multiple-registrant.patch
-pushd feeds/luci
-cat ../../../patches/fullconenat-luci.patch | git apply
-popd
+patch -d feeds/luci -p1 -i ../../../patches/fullconenat-luci.patch
 mkdir -p package/network/config/firewall/patches
 wget -P package/network/config/firewall/patches/ https://github.com/immortalwrt/immortalwrt/raw/openwrt-21.02/package/network/config/firewall/patches/fullconenat.patch
 
-# IPv6 helper
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipv6-helper package/new/ipv6-helper
-
-# PassWall
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/new/luci-app-passwall
-#svn co https://github.com/kiddin9/openwrt-passwall/trunk/luci-app-passwall package/new/luci-app-passwall
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/brook package/new/brook
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/chinadns-ng package/new/chinadns-ng
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/hysteria package/new/hysteria
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go package/new/trojan-go
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus package/new/trojan-plus
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-core package/new/v2ray-core
+# OpenClash
+git clone -b master --depth 1 --single-branch https://github.com/vernesong/OpenClash package/new/luci-app-openclash
 
 # Realtek RTL8811CU/RTL8821CU
 svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/kernel/rtl8821cu package/new/rtl8821cu
@@ -58,22 +46,28 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ramfree 
 # Scheduled Reboot
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-autoreboot package/new/luci-app-autoreboot
 
-# Shared for PassWall and ShadowsocksR Plus+
+# ShadowsocksR Plus+
 rm -rf ./feeds/packages/net/kcptun
 rm -rf ./feeds/packages/net/xray-core
+rm -rf ./feeds/packages/net/shadowsocks-libev
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/new/luci-app-ssr-plus
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2 package/new/redsocks2
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/dns2socks package/new/dns2socks
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipt2socks package/new/ipt2socks
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks package/new/microsocks
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/pdnsd-alt package/new/pdnsd-alt
-#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/simple-obfs package/new/simple-obfs
 svn co https://github.com/coolsnowwolf/packages/trunk/net/kcptun package/new/kcptun
+svn co https://github.com/coolsnowwolf/packages/trunk/net/shadowsocks-libev package/new/shadowsocks-libev
 svn co https://github.com/fw876/helloworld/trunk/naiveproxy package/new/naiveproxy
-svn co https://github.com/fw876/helloworld/trunk/tcping package/new/tcping
-svn co https://github.com/fw876/helloworld/trunk/v2ray-plugin package/new/v2ray-plugin
-#svn co https://github.com/fw876/helloworld/trunk/xray-plugin package/new/xray-plugin
-svn co https://github.com/fw876/helloworld/trunk/xray-core package/new/xray-core
 svn co https://github.com/fw876/helloworld/trunk/shadowsocks-rust package/new/shadowsocks-rust
 svn co https://github.com/fw876/helloworld/trunk/shadowsocksr-libev package/new/shadowsocksr-libev
+svn co https://github.com/fw876/helloworld/trunk/simple-obfs package/new/simple-obfs
+svn co https://github.com/fw876/helloworld/trunk/tcping package/new/tcping
+svn co https://github.com/fw876/helloworld/trunk/trojan package/new/trojan
+svn co https://github.com/fw876/helloworld/trunk/v2ray-plugin package/new/v2ray-plugin
+svn co https://github.com/fw876/helloworld/trunk/xray-core package/new/xray-core
+# building ssr-libev with libmbedtls
+patch -d package/new -p1 -i ../../../patches/building-ssr-libev-with-libmbedtls.patch
 
 # Traffic Usage Monitor
 git clone -b master --depth 1 --single-branch https://github.com/brvphoenix/wrtbwmon package/new/wrtbwmon
