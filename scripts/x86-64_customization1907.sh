550664aba0f68177df7809d6843f3833b0db0293
jinlife <86603404+jinlife@users.noreply.github.com>
2022-03-22 12:27:33 +0800

    Update x86-64_customization1907.sh

--- scripts/x86-64_customization1907.sh
+++ scripts/x86-64_customization1907.sh
@@ -23,7 +23,7 @@ cp -rf ../r8125 package/new/r8125
 
 #svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06/package/kernel/r8152 package/new/r8152
 cp -rf ../r8152 package/new/r8152
-patch -d package/new/r8152/src -p1 -i ../../../r8152/patches/100-add-LED-configuration-from-OF.patch
+patch -d package/new/r8152/src -p1 -i ../r8152/patches/100-add-LED-configuration-from-OF.patch
 
 #Default IP
 sed -i 's#192.168.1.1#192.168.0.1#g' package/base-files/files/bin/config_generate
