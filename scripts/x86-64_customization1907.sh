7e1d57cd812b4f84416cbc1212337f52491f6153
jinlife <86603404+jinlife@users.noreply.github.com>
2022-06-06 00:27:33 +0800

    Update x86-64_customization1907.sh

--- scripts/x86-64_customization1907.sh
+++ scripts/x86-64_customization1907.sh
@@ -14,8 +14,10 @@ ln -sf ../../../feeds/packages/libs/libcap-ng ./package/feeds/packages/libcap-ng
 
 # Openwrt 19.07
 pushd target/linux/generic/hack-4.14
-wget https://github.com/Lienol/openwrt/raw/19.07/target/linux/generic/hack-4.14/952-net-conntrack-events-support-multiple-registrant.patch
-wget https://github.com/Lienol/openwrt/raw/19.07/target/linux/generic/hack-4.14/953-net-patch-linux-kernel-to-support-shortcut-fe.patch
+wget https://github.com/immortalwrt/immortalwrt/raw/openwrt-21.02/target/linux/generic/hack-5.4/952-net-conntrack-events-support-multiple-registrant.patch
+wget https://github.com/immortalwrt/immortalwrt/raw/openwrt-21.02/target/linux/generic/hack-5.4/953-net-patch-linux-kernel-to-support-shortcut-fe.patch
+#wget https://github.com/Lienol/openwrt/raw/19.07/target/linux/generic/hack-4.14/952-net-conntrack-events-support-multiple-registrant.patch
+#wget https://github.com/Lienol/openwrt/raw/19.07/target/linux/generic/hack-4.14/953-net-patch-linux-kernel-to-support-shortcut-fe.patch
 popd
 
 #Default IP
